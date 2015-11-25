9bfc877994cac9c2181e5fbf2cee8d424f0c41de
Jesse Luo <Jesse@Jesses-MacBook-Pro.local>
2015-11-25 14:20:26 -0800

    google maps working

--- foodtruck/static/controller.js
+++ foodtruck/static/controller.js
@@ -10,10 +10,50 @@ app.controller('IndexController', function($http, $window) {
   vm.query = {}
   vm.results = []
   vm.find_closest = function() {
+    console.log(autocomplete.getPlace())
+    var place = autocomplete.getPlace();
+    vm.query.longitude = place.geometry.location.lng();
+    vm.query.latitude = place.geometry.location.lat();
+    vm.query.address = place.formatted_address
     $http.post("/api/find_closest", vm.query).then(
         function successCallBack(response) {
           var data = response.data;
           vm.results = data.nearest_trucks
         })
   }
+
+
+    var mapOptions = {
+      center: {
+        lat: 37.7749295,
+        lng: -122.41941550000001
+      },
+      scrollwheel: false,
+      zoom: 12
+    }
+
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    // Create the autocomplete object, restricting the search to geographical
+    // location types.
+    autocomplete = new google.maps.places.Autocomplete(
+      /** @type {!HTMLInputElement} */
+      (document.getElementById('autocomplete')), {
+        types: ['geocode']
+      });
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var geolocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        var circle = new google.maps.Circle({
+          center: geolocation,
+          radius: position.coords.accuracy
+        });
+        autocomplete.setBounds(circle.getBounds());
+      });
+    }
+    
+
 });
--- foodtruck/urls.py
+++ foodtruck/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^home$', views.get_address),
-    url(r'^api/get_address$', views.get_address),
+    # url(r'^api/get_address$', views.get_address),
     url(r'^api/find_closest$', views.find_closest)
 ]
